fec80c2bfdc7b96425e384f024cf8f28f981a119
Joel Bengs <joel.bengs@gmail.com>
2021-07-28 17:14:06 +0200

    Assignment D8 done

--- src/textproc/GeneralWordCounter.java
+++ src/textproc/GeneralWordCounter.java
@@ -1,2 +1,47 @@
-public class GeneralWordCounter() {
+package textproc;
+import textproc.TextProcessor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class GeneralWordCounter implements TextProcessor {
+    private Map<String, Integer> wordMap;
+    private Set<String> stopWords;
+
+    public GeneralWordCounter(Set wordSet) {
+        wordMap = new HashMap<String, Integer>();
+        stopWords = wordSet;
+    }
+
+    /**
+     * Anropas när ett ord lästs in. Metoden ska uppdatera statistiken därefter.
+     *
+     * @param w
+     */
+    @Override
+    public void process(String w) {
+        if (!stopWords.contains(w)) {
+            if (wordMap.containsKey(w)) {
+                int n = wordMap.get(w);
+                wordMap.put(w, n+1);
+            } else {
+                wordMap.put(w, 1);
+            }
+        }
+    }
+
+    /**
+     * Anropas när samtliga ord i sekvensen lästs in. Metoden ska skriva ut
+     * en sammanställning av statistiken.
+     */
+    @Override
+    public void report() {
+        Set<String> output = wordMap.keySet();
+        for (String s : output) {
+            if (wordMap.get(s) >= 200){
+                System.out.println(s + ": " + wordMap.get(s));
+            }
+        }
+    }
 }
--- src/textproc/Holgersson.java
+++ src/textproc/Holgersson.java
@@ -4,9 +4,7 @@ import org.w3c.dom.Text;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Holgersson {
 
@@ -16,22 +14,33 @@ public class Holgersson {
 			"öland", "östergötland"};
 
 	public static void main(String[] args) throws FileNotFoundException {
-
+		//Creates simple word processors
 		List<TextProcessor> processorList= new ArrayList<TextProcessor>();
 		processorList.add(new SingleWordCounter("nils"));
 		processorList.add(new SingleWordCounter("norge"));
 
+		//Creat multi word processor
 		TextProcessor multiProcessor = new MultiWordCounter(REGIONS);
+
+		//Creates general word counter
+		Scanner scan = new Scanner(new File("undantagsord.txt"));
+		Set<String> stopWords = new HashSet<String>();
+		while(scan.hasNext()) {
+			stopWords.add(scan.next());
+		}
+
+		TextProcessor generalProcessor = new GeneralWordCounter(stopWords);
 
 		Scanner s = new Scanner(new File("nilsholg.txt"));
 		s.findWithinHorizon("\uFEFF", 1);
 		s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
 
+
 		while (s.hasNext()) {
 			String word = s.next().toLowerCase();
 
+			generalProcessor.process(word);
 			multiProcessor.process(word);
-
 			for (TextProcessor p : processorList) {
 				p.process(word);
 			}
@@ -42,22 +51,20 @@ public class Holgersson {
 		for (TextProcessor p : processorList) {
 			p.report();
 		}
-
 		multiProcessor.report();
-
+		generalProcessor.report();
 		feature1();
 		feature2();
 		importantBugFix();
 	}
 
 	public static void feature1() {
-		System.out.println("feature1");
+		System.out.println("feature1 to learn Git");
 	}
-
 	public static void feature2() {
-		System.out.println("feature2");
+		System.out.println("feature2 to learn Git");
 	}
 	public static void importantBugFix() {
-		System.out.println("bugfix");
+		System.out.println("bugfix to learn Git");
 	}
 }
